1b39df4b72fd8cc75d32aff6b4617488e77be2d6
Nigel Schuster <nigel.schusters@googlemail.com>
2015-09-22 23:36:21 -0500

    Dao stubs

---
+++ src/main/java/com/schuster/dao/DepartmentDao.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2015 Nigel Schuster.
+ */
+
+
+package com.schuster.dao;
+
+import com.schuster.entities.Department;
+import com.schuster.generics.UUIDGenericDao;
+
+/**
+ * @author nschuste
+ * @version 1.0.0
+ * @since Sep 22, 2015
+ */
+public class DepartmentDao extends UUIDGenericDao<Department> {
+}
---
+++ src/main/java/com/schuster/dao/InstructorDao.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2015 Nigel Schuster.
+ */
+
+
+package com.schuster.dao;
+
+import com.schuster.entities.Instructor;
+import com.schuster.generics.UUIDGenericDao;
+
+/**
+ * @author nschuste
+ * @version 1.0.0
+ * @since Sep 22, 2015
+ */
+public class InstructorDao extends UUIDGenericDao<Instructor> {
+}
